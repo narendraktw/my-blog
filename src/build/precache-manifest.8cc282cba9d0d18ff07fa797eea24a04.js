36573af1eacdef1cc926aab23ea9ca4f6faf0545
narendraktw <narendraktw@gmail.com>
2020-07-18 19:06:03 +0530

    First Commit

---
+++ src/build/precache-manifest.8cc282cba9d0d18ff07fa797eea24a04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea5c1ae8ab3eaba463ccdbe1e7290548",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76926c9c5c6b465ed120",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "638eb302925180ac35ca",
+    "url": "/static/js/2.9f531c44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9f531c44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76926c9c5c6b465ed120",
+    "url": "/static/js/main.e0cc62dc.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
